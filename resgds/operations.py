1803753b00aaae1e76542d399bb9fcee3fcdf0d9
Gareth Jones <garethsion@googlemail.com>
2018-11-28 16:07:31 +0000

    Going to make translate operatiosn

---
+++ resgds/operations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class Operations:
+    def __init__(self):
+        return
+
+    def mirror(self):
+        return
+
+    def translate(self):
+        return
+
+    def rotate(self):
+        return
